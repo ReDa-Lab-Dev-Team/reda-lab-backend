f786c63bb6b7eb19f2c85b70b9420f03986d5670
Striker Eurika <ngentina69@gmail.com>
2026-02-28 00:41:51 +0700

    final dockerization configuration

--- app/config/config.py
+++ app/config/config.py
@@ -1,25 +1,31 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
 
 
 class Settings(BaseSettings):
+    # Database settings
     database_hostname: str
     database_port: str
     database_password: str
     database_name: str
     database_username: str
+    
+    # Auth settings
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
     
-    # metadata uploads
-    upload_dir: str = Field(default="upload", env="UPLOAD_DIR")
-    class Config:
-        env_file = ".env"
-        case_sensitive = False
-        extra = "ignore"  # Ignore extra env vars not defined in Settings
-        
+    # File uploads
+    upload_dir: str = Field(default="upload", validation_alias="UPLOAD_DIR")
+    
+    # Pydantic v2 config
+    model_config = SettingsConfigDict(
+        # env_file=".env",
+        case_sensitive=False,
+        extra="ignore",  # Ignore undefined env vars
+        env_prefix=""    # No prefix required
+    )
 
 settings = Settings()
 # ENVIRONMENT=development  # Change to "production" when deploying
