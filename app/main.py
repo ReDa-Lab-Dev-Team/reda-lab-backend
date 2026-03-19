651e2ae848dbc9ac886d5f0b9384f052f9648b25
Kheangzz <kheangyeng61@gmail.com>
2026-03-19 16:02:37 +0700

    allow front

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://localhost:5173",
+        "http://localhost:5173",
         "https://reda.ams.cards/"
     ],
     allow_credentials=True,
